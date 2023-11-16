f7d423f48016f915a91b5e4a53623e0375b7c319
Code Sandbox <hello@codesandbox.io>
2023-11-16 13:03:02 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.5cf90db8d2036005958e880e6689a12a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e5d3b04062919d46698738c3110f606",
+    "url": "https://yancymin.github.io/csb-443xwc/index.html"
+  },
+  {
+    "revision": "73460c68a39bb0fc3718",
+    "url": "https://yancymin.github.io/csb-443xwc/static/css/2.47cdfd64.chunk.css"
+  },
+  {
+    "revision": "73460c68a39bb0fc3718",
+    "url": "https://yancymin.github.io/csb-443xwc/static/js/2.07ce2029.chunk.js"
+  },
+  {
+    "revision": "21189b1ba7e66c9adb27",
+    "url": "https://yancymin.github.io/csb-443xwc/static/js/main.c1337bf8.chunk.js"
+  },
+  {
+    "revision": "ac86fbf232336dc81bae",
+    "url": "https://yancymin.github.io/csb-443xwc/static/js/runtime~main.b8641f74.js"
+  }
+]);
